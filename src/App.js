e9431d88637aebb79989355631d9928b56f6b79b
IslamNabi <islamnabics@gmail.com>
2024-06-12 21:57:43 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import ImageSlider from './Components/ImageSlider';
+import Beach from './images/Beach.jpg';
+import Greenery from './images/Greenery.jpg';
+import Mountains from './images/mountains.jpg';
+import city from './images/City.jpg';
 import './App.css';
 
 function App() {
+  const slides = [
+    { url: Beach, title: 'Beach' },
+    { url: Greenery, title: 'Green' },
+    { url: Mountains, title: 'Mountains' },
+    { url: city, title: 'City' }
+  ];
+
+  const containerStyles = {
+    width: '600px',
+    height: '300px',
+    margin: '0 auto'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ textAlign: 'center' }}>
+      <h1>Image Slider</h1>
+      </div>
+        <div style={containerStyles}>
+          <ImageSlider slides={slides} />
+        </div>
     </div>
   );
 }
